afe2c7a46cd466d8b7c5e4a1e1b0bdf8d610ed10
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-22 16:28:43 +0600

    Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
 // import TemplateSyntax from './components/TemplateSyntax.vue'
-import ClassAndStyleBindings from './components/ClassAndStyleBindings.vue'
+// import ClassAndStyleBindings from './components/ClassAndStyleBindings.vue'
+import ConditionalRendering from './components/ConditionalRendering.vue'
 </script>
 
 <template>
   <header>
     <!-- <h2>Template Syntax</h2> -->
-    <h2>Class and Style Bindings</h2>
+    <!-- <h2>Class and Style Bindings</h2> -->
+    <h2>Conditional Rendering</h2>
   </header>
 
   <main>
     <!-- <TemplateSyntax /> -->
-    <ClassAndStyleBindings />
+    <!-- <ClassAndStyleBindings /> -->
+    <ConditionalRendering />
   </main>
 </template>
 
---
+++ src/components/ConditionalRendering.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+        awesome: true,
+        ok: true,
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<template>
+
+    <button @click="awesome = !awesome">Toggle</button>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+
+    <hr>
+
+    <template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+    </template>
+
+    <hr>
+
+    <h1 v-show="ok">Hello Show!</h1>
+
+</template>
+
+<style scoped>
+
+</style>
