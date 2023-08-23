d7b6d7b2f5aef28b15a8b48ec08caa5ab5b22130
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-23 16:13:40 +0600

    Event Handling

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 // import TemplateSyntax from './components/TemplateSyntax.vue'
 // import ClassAndStyleBindings from './components/ClassAndStyleBindings.vue'
 // import ConditionalRendering from './components/ConditionalRendering.vue'
-import ListRendering from './components/ListRendering.vue'
+// import ListRendering from './components/ListRendering.vue'
+import EventHandling from './components/EventHandling.vue'
 </script>
 
 <template>
@@ -10,14 +11,16 @@ import ListRendering from './components/ListRendering.vue'
     <!-- <h2>Template Syntax</h2> -->
     <!-- <h2>Class and Style Bindings</h2> -->
     <!-- <h2>Conditional Rendering</h2> -->
-    <h2>List Rendering</h2>
+    <!-- <h2>List Rendering</h2> -->
+    <h2>Event Handling</h2>
   </header>
 
   <main>
     <!-- <TemplateSyntax /> -->
     <!-- <ClassAndStyleBindings /> -->
     <!-- <ConditionalRendering /> -->
-    <ListRendering />
+    <!-- <ListRendering /> -->
+    <EventHandling />
   </main>
 </template>
 
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+  data() {
+    return {
+        count: '0',
+        inputValue: '',
+        inputResult: '',
+        inputResult: '',
+        selfCount: '0',
+        keyupInputValue: '',
+        keyupInputResult: '',
+    };
+  },
+  methods: {
+    say(name) {
+        alert(name)
+    },
+    sayEvent(name, event) {
+        event.preventDefault();
+        alert(name)
+        console.log(event.type)
+    },
+    doThis(){
+        alert('Event Modifiers')
+    },
+    onSubmit(){
+        this.inputResult = this.inputValue
+    },
+    doThatBox(){
+        this.selfCount++
+    },
+    submit(){
+        this.keyupInputResult = this.keyupInputValue
+    }
+  }
+};
+</script>
+
+<template>
+
+<h4>Inline Handlers</h4>
+<button @click="count++">+</button>
+<span>Count is: {{ count }}</span>
+<button @click="count--">-</button>
+
+<hr>
+
+<h4>Method Handlers</h4>
+<button @click="say('Sabbir')">Say hello</button>
+<button @click="sayEvent('Sabbir', $event)">Say hello</button>
+
+<hr>
+
+<h4>Event Modifiers</h4>
+<a @click.stop="doThis">Event Modifiers</a>
+<form @submit.prevent="onSubmit">
+    <input type="text"  v-model="inputValue">
+    <button type="submit">Input</button>
+    <span>{{ inputResult }}</span>
+</form>
+
+<hr>
+
+<div class="box" @click.self="doThatBox">
+    Click Main Div
+    <div class="child-box">
+        {{ selfCount }}
+    </div>
+</div>
+
+
+<hr>
+<h4>Key Modifiers</h4>
+<input type="text" @keyup.enter="submit" v-model="keyupInputValue">
+<span>{{ keyupInputResult }}</span>
+
+<hr>
+<h4>Mouse Button Modifiers</h4>
+<div class="box" @click.right="doThatBox">
+    Right Click {{ selfCount }}
+</div>
+
+</template>
+
+<style scoped>
+.box{
+    width: 100px;
+    height: 100px;
+    background-color: aqua;
+    color: black;
+}
+.child-box{
+    width: 80px;
+    height: 80px;
+    background-color: rgb(0, 255, 34);
+    color: black;
+}
+</style>
